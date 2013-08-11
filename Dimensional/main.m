927a2564a05e48322183a7dc12956eeafeebc80b
Iggy.Krajci <iggy.krajci@cummings-inc.com>
2013-08-11 16:44:43 -0700

    Initial Import of iOS game

---
+++ Dimensional/main.m
@@ -0,0 +1,17 @@
+//
+//  main.m
+//  Dimensional
+//
+//  Created by Iggy Krajci on 8/11/13.
+//  Copyright Iggy Krajci 2013. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+int main(int argc, char *argv[]) {
+    
+    NSAutoreleasePool * pool = [[NSAutoreleasePool alloc] init];
+    int retVal = UIApplicationMain(argc, argv, nil, @"AppController");
+    [pool release];
+    return retVal;
+}
